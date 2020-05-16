ecb2d18e4730dabc28aaf4b4d330e33464567fdc
Pykälämäki <juha.pykalamaki@kavokerr.com>
2020-05-16 14:05:11 +0300

    3.1 puhelinluettelon backend step1, 3.2: puhelinluettelon backend step2, 3.3: puhelinluettelon backend step3, 3.4: puhelinluettelon backend step4, 3.5: puhelinluettelon backend step5, 3.6: puhelinluettelon backend step6

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const minId = 0
+const maxId = 100000
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Phonebook</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.get('/info', (request, response) => {
+    let message = `<div>Phonebook has info for ${persons.length} people</div>`
+    message += `<div>${new Date()}</div>`
+    response.send(message)
+})
+
+const generateId = () => {
+    min = Math.ceil(minId);
+    max = Math.floor(maxId);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+app.post('/api/persons', (request, response) => {
+    console.log(request.body)
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name/number is missing.'
+        })
+    }
+
+    const existingPerson = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
+    if (existingPerson) {
+        return response.status(400).json({
+            error: 'name must be unique.'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
