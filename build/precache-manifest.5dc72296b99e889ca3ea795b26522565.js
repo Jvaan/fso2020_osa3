2ba30c28959830cd4b9be36c9718d1065063f9b5
Juha Pykälämäki <juha.pykalamaki@kavokerr.com>
2020-05-22 23:46:03 +0300

    3.9 puhelinluettelon backend step9, 3.10 puhelinluettelon backend step10

---
+++ build/precache-manifest.5dc72296b99e889ca3ea795b26522565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223b9f39baf072b1c53adfe62d06a061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82fa773951fe0a7cda92",
+    "url": "/static/css/main.b218ba28.chunk.css"
+  },
+  {
+    "revision": "d9189b8ec77a982c425a",
+    "url": "/static/js/2.d92478fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d92478fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82fa773951fe0a7cda92",
+    "url": "/static/js/main.6bbb9069.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function(req) {
     return JSON.stringify(req.body)
 })
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -100,7 +103,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
