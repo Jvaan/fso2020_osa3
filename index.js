c808416c463484b8293ff899b4f37ad5a027a539
Pykälämäki <juha.pykalamaki@kavokerr.com>
2020-05-26 00:57:41 +0300

    3.13: puhelinluettelo ja tietokanta, step1, 3.14: puhelinluettelo ja tietokanta, step2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 morgan.token('body', function(req) {
@@ -15,35 +17,14 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 const minId = 0
 const maxId = 100000
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/', (req, res) => {
     res.send('<h1>Phonebook</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+      })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -85,22 +66,23 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    /*
     const existingPerson = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
     if (existingPerson) {
         return response.status(400).json({
             error: 'name must be unique.'
         })
     }
+    */
 
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+      })
 })
 
 const PORT = process.env.PORT || 3001
