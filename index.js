006c7a6eaa1a950041900e0d104d04e60ac5c5cf
Pykälämäki <juha.pykalamaki@kavokerr.com>
2020-05-26 01:49:24 +0300

    3.15-3.18: puhelinluettelo ja tietokanta, steps 3-6

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const Person = require('./models/person')
 const app = express()
 
-morgan.token('body', function(req) {
+morgan.token('body', function (req) {
     return JSON.stringify(req.body)
 })
 
@@ -14,9 +14,6 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const minId = 0
-const maxId = 100000
-
 app.get('/', (req, res) => {
     res.send('<h1>Phonebook</h1>')
 })
@@ -24,40 +21,39 @@ app.get('/', (req, res) => {
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons)
-      })
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    let message = `<div>Phonebook has info for ${persons.length} people</div>`
-    message += `<div>${new Date()}</div>`
-    response.send(message)
+app.get('/info', (request, response, next) => {
+    Person.find({}).then(persons => {
+        let message = `<div>Phonebook has info for ${persons.length} people</div>`
+        message += `<div>${new Date()}</div>`
+        response.send(message)
+    })
+        .catch(error => next(error))
 })
 
-const generateId = () => {
-    min = Math.ceil(minId);
-    max = Math.floor(maxId);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -82,8 +78,42 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
-      })
+    })
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person)
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+mongoose.set('useFindAndModify', false)
+
 const url = process.env.MONGODB_PHONEBOOK_URL
 // `mongodb+srv://fullstack_test1:${password}@cluster0-qngkz.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
